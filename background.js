be3b655715f92e2b9a52867cb056ce8cce19fa7c
FoxTheWolf <caiocvlmota@gmail.com>
2020-07-01 12:10:47 -0300

    Beautified code

--- background.js
+++ background.js
@@ -1,54 +1,63 @@
 // Logs that the extension has been loaded
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({color: '#3aa757'}, function () {
-      console.log("scamcheck is running");
-    });
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({
+    color: '#3aa757'
+  }, function() {
+    console.log("scamcheck is running");
   });
+});
 
 // Declares the url variable
 var url;
 
 // This script runs every time a tab is loading an url
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'loading' && tab.active) {
-      chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-          let url = tabs[0].url; // We add the url being loaded to the url variable
-          console.log(url);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == 'loading' && tab.active) {
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, tabs => {
+      let url = tabs[0].url; // We add the url being loaded to the url variable
+      console.log(url);
 
-          const form = { // This is a basic form that is sent to the safe browsing api
-            client: {
-              clientId:      'yourcompanyname',
-              clientVersion: '1.5.2'
-            },
-            threatInfo: {
-              threatTypes:      ['MALWARE', 'SOCIAL_ENGINEERING'],
-              platformTypes:    ['WINDOWS'],
-              threatEntryTypes: ['URL'],
-              threatEntries: [
-                {'url': url},
-              ]
-            }
-          };
+      const form = { // This is a basic form that is sent to the safe browsing api
+        client: {
+          clientId: 'yourcompanyname',
+          clientVersion: '1.5.2'
+        },
+        threatInfo: {
+          threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING'],
+          platformTypes: ['WINDOWS'],
+          threatEntryTypes: ['URL'],
+          threatEntries: [{
+            'url': url
+          }, ]
+        }
+      };
 
-          const safeBrowsing = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyBJ0hstpWW4kq4torS5SFnxsNhTXZY1wEs" // The safe browsing url and the api key
-          const safeBrowsingParams = { // Essential parameters sent with the request
-            headers:{
-              "content-type": "application/json; charset=UTF-8"
-            },
-            body:JSON.stringify(form),
-            method:"POST"
-          }
+      const safeBrowsing = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyBJ0hstpWW4kq4torS5SFnxsNhTXZY1wEs" // The safe browsing url and the api key
+      const safeBrowsingParams = { // Essential parameters sent with the request
+        headers: {
+          "content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(form),
+        method: "POST"
+      }
 
 
-          fetch(safeBrowsing,safeBrowsingParams) // We use fetch to make the api call
-          .then(response => {return response.json()})
-          .then(data => {console.log(data)
-          if(data.matches){ // If the api returns an undefined object, the website is safe. If not, it has a match
+      fetch(safeBrowsing, safeBrowsingParams) // We use fetch to make the api call
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          if (data.matches) { // If the api returns an undefined object, the website is safe. If not, it has a match
             alert("This website may contain a scam. Tread carefully!"); // TODO: improve the warning message
-          }})
-          .catch(
-              err => console.log(err)
-          )
-      });
-    };
-  });
+          }
+        })
+        .catch(
+          err => console.log(err)
+        )
+    });
+  };
+});
