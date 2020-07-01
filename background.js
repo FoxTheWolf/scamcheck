b924a7ae331f5966ef82031bd71bb0743e43513c
FoxTheWolf <caiocvlmota@gmail.com>
2020-07-01 12:03:28 -0300

    Added basic Safe Browsing API functionality

--- background.js
+++ background.js
@@ -1,41 +1,54 @@
+// Logs that the extension has been loaded
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({color: '#3aa757'}, function () {
-      console.log("The color is green.");
+      console.log("scamcheck is running");
     });
   });
+
+// Declares the url variable
+var url;
+
+// This script runs every time a tab is loading an url
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
+    if (changeInfo.status == 'loading' && tab.active) {
       chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-          let url = tabs[0].url;
+          let url = tabs[0].url; // We add the url being loaded to the url variable
           console.log(url);
+
+          const form = { // This is a basic form that is sent to the safe browsing api
+            client: {
+              clientId:      'yourcompanyname',
+              clientVersion: '1.5.2'
+            },
+            threatInfo: {
+              threatTypes:      ['MALWARE', 'SOCIAL_ENGINEERING'],
+              platformTypes:    ['WINDOWS'],
+              threatEntryTypes: ['URL'],
+              threatEntries: [
+                {'url': url},
+              ]
+            }
+          };
+
+          const safeBrowsing = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyBJ0hstpWW4kq4torS5SFnxsNhTXZY1wEs" // The safe browsing url and the api key
+          const safeBrowsingParams = { // Essential parameters sent with the request
+            headers:{
+              "content-type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify(form),
+            method:"POST"
+          }
+
+
+          fetch(safeBrowsing,safeBrowsingParams) // We use fetch to make the api call
+          .then(response => {return response.json()})
+          .then(data => {console.log(data)
+          if(data.matches){ // If the api returns an undefined object, the website is safe. If not, it has a match
+            alert("This website may contain a scam. Tread carefully!"); // TODO: improve the warning message
+          }})
+          .catch(
+              err => console.log(err)
+          )
       });
     };
   });
-
-var form;
-
-form.onsubmit = function (e) {
-  // stop the regular form submission
-  e.preventDefault();
-
-  // collect the form data while iterating over the inputs
-  var data = {};
-  for (var i = 0, ii = form.length; i < ii; ++i) {
-    var input = form[i];
-    if (input.name) {
-      data[input.name] = input.value;
-    }
-  }
-
-  // construct an HTTP request
-  var xhr = new XMLHttpRequest();
-  xhr.open(form.method, form.action, true);
-  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-  // send the collected data as JSON
-  xhr.send(JSON.stringify(data));
-
-  xhr.onloadend = function () {
-    // done
-  };
-};
